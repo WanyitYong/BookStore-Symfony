462474d78f27d0218409276edd304a62e1b5e19f
Wan Yit Yong <B00100202@student.itb.ie>
2019-04-04 19:56:03 +0100

    login

--- BookStore/src/Security/AccessDeniedHandler.php
+++ BookStore/src/Security/AccessDeniedHandler.php
@@ -1,6 +1,8 @@
 <?php
 namespace App\Security;
 
+use Psr\Container\ContainerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AccessDeniedException;
@@ -8,8 +10,22 @@ use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
 
 class AccessDeniedHandler implements AccessDeniedHandlerInterface
 {
+    private $twig;
+    private $logger;
+
+    public function __construct(ContainerInterface $container, LoggerInterface $logger)
+    {
+        $this->twig = $container->get('twig');
+        $this->logger = $logger;
+    }
+
     public function handle(Request $request, AccessDeniedException $accessDeniedException)
     {
-        return new Response('Sorry, you have been denied access', 403);
+        $this->logger->error('Access Denied Exception');
+
+        $template = 'error/accessDenied.html.twig';
+        $args = [];
+        $html = $this->twig->render($template, $args);
+        return new Response($html);
     }
 }
